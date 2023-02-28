dc76546b030a36bfe1c50c176659d2edda69add4
AngeloRoudani <ange.rouda@gmail.com>
2023-02-28 14:43:07 +0100

    primo bozza html css

---
+++ js/main.js
@@ -0,0 +1,7 @@
+/* 1 creare un array di immagini.
+   2 creare un ciclo per cui un immagine sostituisca l'altra
+   3 creare un due button in cui al click scorre le immagini  avanti e indietro
+*/
+
+const containerDom = document.querySelector('container')
+
