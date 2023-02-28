3918d01d7efe29ea96623086a6794f3ddcb53af0
AngeloRoudani <ange.rouda@gmail.com>
2023-02-28 15:00:49 +0100

    creato array immagini

--- js/main.js
+++ js/main.js
@@ -1,7 +1,17 @@
 /* 1 creare un array di immagini.
-   2 creare un ciclo per cui un immagine sostituisca l'altra
-   3 creare un due button in cui al click scorre le immagini  avanti e indietro
+   2 immettere le immagini nel DOM 
+   3 creare un ciclo per cui un immagine sostituisca l'altra
+   4 creare un due button in cui al click scorre le immagini  avanti e indietro
 */
 
-const containerDom = document.querySelector('container')
+const imagesList = []
 
+for ( i = 1; i <= 5; i++) {
+    imagesList.push('img/'+ i +'.webp')
+}
+
+const containerDom = document.querySelector('container');
+
+let sliderDom = "";
+
+for ( i = 1; i <= imagesList.lenght; i++)
